927b000d1a60252d974f57beddfab2af87e26c77
Phillip Booth <phillip.booth2015@gmail.com>
2020-01-20 14:52:49 -0500

    adding fixed footer to bottom of application with Milliken logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './layout/top-nav/top-nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
